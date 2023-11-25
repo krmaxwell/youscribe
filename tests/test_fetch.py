13612bb7e184adefe41c58cea2082b2117ac6cea
Kyle Maxwell <kylemxl@icloud.com>
2023-11-25 16:53:18 -0600

    Save URL to file

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,5 +1,5 @@
 import pytest
-from youscribe import get_yt_code
+from youscribe import get_yt_code, save_url_to_file, get_transcript_for_file
 
 
 def test_get_yt_code_regular_url():
@@ -22,3 +22,14 @@ def test_get_yt_code_shared_url():
 def test_get_yt_code_not_youtube_url():
     with pytest.raises(ValueError):
         get_yt_code("https://www.google.com")
+
+
+@pytest.mark.skip(reason="Network request")
+def test_save_url_to_file():
+    url = "https://www.youtube.com/watch?v=5La8Ke35drk"
+    assert save_url_to_file(url) == "5La8Ke35drk.mp4"
+
+
+def test_get_transcript_for_file():
+    file_name = "5La8Ke35drk.mp4"
+    assert get_transcript_for_file(file_name) == "Hello, world!"
--- youscribe.py
+++ youscribe.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlparse
+from pytube import YouTube
 
 
 def get_yt_code(url: str):
@@ -14,3 +15,15 @@ def get_yt_code(url: str):
     else:
         raise ValueError("URL must be a YouTube URL")
     return video_id
+
+
+def save_url_to_file(url: str):
+    video_id = get_yt_code(url)
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True)[0]
+    audio_stream.download(filename=f"{video_id}.mp4")
+    return f"{video_id}.mp4"
+
+
+def get_transcript_for_file(file_name: str):
+    return "Hello, world!"
